22fbc05d1de01d9eb29bcff500b1a0c94b53f0d6
Jonathan Beluch <jon@jonathanbeluch.com>
2014-03-04 15:57:13 -0700

    testing something

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-
-setup(
-    name='hackandtell_boulder',
-    description='Hack and Tell Boulder docs',
-    packages=find_packages(),
-    version='0.1',
-)
