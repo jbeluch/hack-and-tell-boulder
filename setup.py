d2496891fa30c308f11954bebbc770ff071c8834
Jonathan Beluch <jon@jonathanbeluch.com>
2014-03-03 18:55:09 -0700

    try an actual package

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 setup(
     name='hackandtell_boulder',
     description='Hack and Tell Boulder docs',
+    packages=find_packages(),
     version='0.1',
 )
